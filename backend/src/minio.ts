0c6d44db7569b7af25df8251d72f3e27fd11b91c
Tamás Szabó <tamas@szaboo.com>
2025-07-03 23:27:07 +0200

    add minio controller

--- backend/src/minio.ts
+++ backend/src/minio.ts
@@ -1,6 +1,9 @@
 import { Client } from 'minio'
 
-const { MINIO_USER, MINIO_PASSWORD, MINIO_ENDPOINT, MINIO_PORT } = process.env
+const { MINIO_USER, MINIO_PASSWORD, MINIO_ENDPOINT, MINIO_PORT, MINIO_BUCKET } =
+  process.env
+
+export const bucket = MINIO_BUCKET || 'inventoria'
 
 const minioClient = new Client({
   endPoint: MINIO_ENDPOINT || 'localhost',
@@ -9,5 +12,7 @@ const minioClient = new Client({
   accessKey: MINIO_USER,
   secretKey: MINIO_PASSWORD,
 })
+
+// TODO: periodically delete incomplete uploads
 
 export default minioClient
