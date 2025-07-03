5f5960241f3559a8c636fac82b7016887d6f00cc
Tamás Szabó <tamas@szaboo.com>
2025-07-03 23:46:42 +0200

    add rate limiting for off

--- backend/src/routes/items.ts
+++ backend/src/routes/items.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express'
+import { rateLimit } from 'express-rate-limit'
 import * as itemsController from '../controllers/item'
 
 const router = Router()
 
+const productReadLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 90, // Limit to 90 requests per minute actual is 100
+  message: { message: 'Too many requests, please try again later.' },
+})
+
 router.get('/', itemsController.getItems)
-router.post('/', itemsController.createItem)
+router.post('/', productReadLimiter, itemsController.createItem)
 router.get('/:id', itemsController.getItem)
 router.delete('/:id', itemsController.deleteItem)
 router.put('/:id', itemsController.updateItem)
--- backend/src/routes/off.ts
+++ backend/src/routes/off.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express'
+import { rateLimit } from 'express-rate-limit'
 import * as openFoodFactsController from '../controllers/OpenFoodFacts'
 
 const router = Router()
 
-router.get('/search/:term', openFoodFactsController.search)
+const searchLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 2, // Limit to 8 requests per minute actual is 10
+  message: { message: 'Too many search requests, please try again later.' },
+})
+
+router.get('/search/:term', searchLimiter, openFoodFactsController.search)
 
 export default router
