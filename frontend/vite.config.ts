70fa0d6b53217d6dd2609c4675bddaaaa7e32c89
Tamás Szabó <tamas@szaboo.com>
2025-07-04 16:26:15 +0200

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+import { config } from 'dotenv'
+config()
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.BACKEND_URL || 'http://localhost:3000',
         changeOrigin: true,
       },
     },
