acc6519eb4ac8f1e93fc76dcf2ec8138f8461410
Tamás Szabó <tamas@szaboo.com>
2025-07-20 01:29:31 +0200

    remove duplicate devtool

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
   component: () => (
@@ -10,7 +9,6 @@ export const Route = createRootRoute({
         </Link>
       </div>
       <Outlet />
-      <TanStackRouterDevtools />
     </>
   ),
 })
