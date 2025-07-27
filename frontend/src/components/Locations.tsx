996e1b7086015bcef91fc94b0c46cf4305467fb8
Tamás Szabó <tamas@szaboo.com>
2025-07-27 18:12:29 +0200

    fix location display

--- frontend/src/components/Locations.tsx
+++ frontend/src/components/Locations.tsx
@@ -1,15 +1,19 @@
 import { getRouteApi } from '@tanstack/react-router'
 import Location from './Location'
 import type { Location as LocationType } from './LocationSelect'
+import { useDialog } from '@/hooks/useDialog'
+import EditLocationDialog from './EditLocationDialog'
 
 const route = getRouteApi('/locations/')
 
 const Locations = () => {
   const data = route.useLoaderData()
 
+  useDialog(EditLocationDialog, 'edit')
+
   return (
-    <div className="relative overflow-hidden w-full p-4">
-      <div className="grid grid-cols-1 gap-4">
+    <div className="w-full p-4">
+      <div className="grid grid-cols-1 gap-4 w-full">
         {data.locations?.map((location: LocationType) => (
           <Location location={location} key={location.id} />
         ))}
