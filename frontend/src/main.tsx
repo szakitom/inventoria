3388a6c0af1e6690f7ed3a3c0a8dcbcd7ba0b51d
Tamás Szabó <tamas@szaboo.com>
2025-08-02 04:37:40 +0200

    add initial dark mode support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,8 @@ import './index.css'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+import { ThemeProvider } from '@components/ui/theme-provider'
+import { ModeToggle } from '@components/ui/mode-toggle'
 
 // Create a new router instance
 const router = createRouter({ routeTree })
@@ -25,9 +27,12 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
-      <TanStackRouterDevtools router={router} />
-      <Toaster richColors />
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <TanStackRouterDevtools router={router} />
+        <Toaster richColors />
+        <ModeToggle />
+      </ThemeProvider>
     </StrictMode>
   )
 }
